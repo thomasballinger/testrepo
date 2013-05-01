f91d1cdc96ad6bb01c02c57067537718701f45a4
Thomas Ballinger <thomasballinger@gmail.com>
2013-05-01 18:46:12 -0400

    added two lines to comment

--- versionControlTest/AppDelegate.h
+++ versionControlTest/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by Thomas Ballinger on 5/1/13.
 //  Copyright (c) 2013 Thomas Ballinger. All rights reserved.
 //
+//  adding a line or two
+//  the second line
 
 #import <UIKit/UIKit.h>
 
